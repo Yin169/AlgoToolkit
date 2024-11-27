191c4e870ae21618c8b03dae45f237052a923972
Yin Cheang Ng <ngyc04169@gmail.com>
2024-11-27 19:55:42 +0800

    update

--- script_test.sh
+++ script_test.sh
@@ -1,4 +1,7 @@
 conan install . --build=missing
 bazel clean
 bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //Obj:matrix_obj_test
-bazel test //Obj:matrix_obj_test
+bazel test --test_output=all //Obj:matrix_obj_test
+
+bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //LinearAlgebra/Factorized:basic_test
+bazel test --test_output=all //LinearAlgebra/Factorized:basic_test
