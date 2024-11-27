d092713e2d7d45ab630c65915f161572d70461e4
Yin Cheang Ng <ngyc04169@gmail.com>
2024-11-28 02:12:45 +0800

    fix bug passed test

--- script_test.sh
+++ script_test.sh
@@ -4,5 +4,5 @@ bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //Obj:matrix_ob
 bazel test --test_output=all --test_verbose_timeout_warnings //Obj:matrix_obj_test
 bazel test --test_output=all --test_verbose_timeout_warnings //Obj:matrix_benchmark_test
 
-# bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //LinearAlgebra/Factorized:basic_test
-# bazel test --test_output=all //LinearAlgebra/Factorized:basic_test
+bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //LinearAlgebra/Factorized:basic_test
+bazel test --test_output=all //LinearAlgebra/Factorized:basic_test
