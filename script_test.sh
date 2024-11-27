6e699aaa82ca94b13f830ff30a52af20d4986137
Yin Cheang Ng <ngyc04169@gmail.com>
2024-11-28 02:02:38 +0800

    check memory leak

--- script_test.sh
+++ script_test.sh
@@ -1,8 +1,8 @@
 conan install . --build=missing
 bazel clean
 bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //Obj:matrix_obj_test
-bazel test --test_output=all //Obj:matrix_obj_test
-bazel test --test_output=all //Obj:matrix_benchmark_test
+bazel test --test_output=all --test_verbose_timeout_warnings //Obj:matrix_obj_test
+bazel test --test_output=all --test_verbose_timeout_warnings //Obj:matrix_benchmark_test
 
 # bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //LinearAlgebra/Factorized:basic_test
 # bazel test --test_output=all //LinearAlgebra/Factorized:basic_test
