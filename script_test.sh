75d2f9630ab6aac06cb3367cc1966fc1753ba6c0
Yin Cheang Ng <ngyc04169@gmail.com>
2024-11-27 19:11:26 +0800

    backup

--- script_test.sh
+++ script_test.sh
@@ -1,3 +1,4 @@
 conan install . --build=missing
 bazel clean
-bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //main:demo
+bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //Obj:matrix_obj_test
+bazel test //Obj:matrix_obj_test
