bb8e9caf8f996c0e9a9b1ce13e3c4f50afb8f2be
Yin Cheang Ng <ngyc04169@gmail.com>
2024-11-28 02:21:33 +0800

    updated

--- script_test.sh
+++ script_test.sh
@@ -6,3 +6,4 @@ bazel test --test_output=all --test_verbose_timeout_warnings //Obj:matrix_benchm
 
 bazel --bazelrc=./conan/conan_bzl.rc build --config=conan-config //LinearAlgebra/Factorized:basic_test
 bazel test --test_output=all //LinearAlgebra/Factorized:basic_test
+bazel test --test_output=all //LinearAlgebra/Factorized:basic_benchmark_test
