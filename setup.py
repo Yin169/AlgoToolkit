deb04ac841d8a61100fa0d69d539ddcdd55a078a
Yin Cheang Ng <ngyc04169@gmail.com>
2025-01-10 20:13:30 +0800

    update file

--- setup.py
+++ setup.py
@@ -14,37 +14,44 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         
-        # Get OpenBLAS path from brew
-        openblas_path = subprocess.check_output(['brew', '--prefix', 'openblas']).decode('utf-8').strip()
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+            
+        # Install dependencies with conan
+        subprocess.check_call(['conan', 'install', '.', '--build=missing'])
         
         cmake_args = [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
             f'-DPYTHON_EXECUTABLE={sys.executable}',
             f'-DCMAKE_BUILD_TYPE=Release',
-            f'-DOpenBLAS_HOME={openblas_path}',
+            f'-DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake',
+            '-DBUILD_PYTHON_BINDINGS=ON'
         ]
-        
-        build_args = ['--config', 'Release']
-        
+
         if platform.system() == "Darwin":
             cmake_args += ['-DCMAKE_OSX_DEPLOYMENT_TARGET=10.15']
-            build_args += ['--', '-j2']
-        
-        build_temp = os.path.join(self.build_temp, ext.name)
-        if not os.path.exists(build_temp):
-            os.makedirs(build_temp)
-            
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_temp)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=build_temp)
+
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', self.build_temp, '--config', 'Release'])
 
 setup(
     name='fastsolver',
     version='0.1.0',
-    author='Your Name',
+    author='NG YIN CHEANG',
     description='Fast numerical solver library',
-    long_description='',
-    ext_modules=[CMakeExtension('fastsolver')],
-    cmdclass=dict(build_ext=CMakeBuild),
-    zip_safe=False,
+    packages=['fastsolver'],
+    package_dir={'fastsolver': 'python'},
+    ext_modules=[CMakeExtension('fastsolver.core')],
+    cmdclass={'build_ext': CMakeBuild},
     python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.15.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C++',
+    ],
 )
